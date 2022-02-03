f2a8a19e8a054e69e9de8fd6a79033a6cbcaa3d4
jpanaia@gmail.com <jpanaia@gmail.com>
2022-02-03 10:38:28 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import {Route, Routes} from "react-router-dom"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+import './styles.css'
+
+function App() {    
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Photos />} />
+                <Route path="/cart" element={<Cart />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default App
