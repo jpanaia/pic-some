77bba88de8eb4c6817807464a657bb54481d1422
jpanaia@gmail.com <jpanaia@gmail.com>
2022-03-01 11:40:39 -0500

    cartItem page setup

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,11 +7,12 @@ function Cart() {
     const cartItemElements = cartItems.map(item => (
         <CartItem key={item.id} item ={item}/>
     ))
+    const totalCost = (cartItems.length * 5.99).toLocaleString("en-US", {style: "currency", currency: "USD"})
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total: {totalCost}</p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>
