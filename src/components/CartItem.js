2fd4bd3a3cea16590b435c8dcf7fc9f8eeb42d90
jpanaia@gmail.com <jpanaia@gmail.com>
2022-03-08 09:26:03 -0500

    some ui touches

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,27 +1,30 @@
-import React, {useContext, useState} from "react"
+import React, {useContext} from "react"
+import PropTypes from 'prop-types'
 import {Context} from '../Context'
+import useHover from '../hooks/useHover'
 
 function CartItem({item}) {
     const {removeImageFromCart} = useContext(Context)
-    const [isHovered, setIsHovered] = useState(false)
-    const trashClass = isHovered ? "fill" : "line"
-
-    function toggleTrashIcon() {
-        setIsHovered(prevState => !prevState)
-    }
+    const [hovered, ref] = useHover()
+    const trashClass = hovered ? "fill" : "line"
 
     return ( 
         <div className="cart-item">
             <i 
                 className={`ri-delete-bin-${trashClass}`} 
-                onMouseEnter={toggleTrashIcon} 
-                onMouseLeave={toggleTrashIcon} 
+                ref= {ref}
                 onClick={()=>removeImageFromCart(item)}
             ></i>
             <img src={item.url} width="130px" />
             <p>$5.99</p>
         </div>
     )
+}
+
+CartItem.propTypes = {
+    item: PropTypes.shape({
+        url: PropTypes.string.isRequired
+    })
 }
 
 export default CartItem
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,30 +1,35 @@
-import React, {useState, useContext} from "react"
+import React, {useContext} from "react"
 import PropTypes from 'prop-types'
 import {Context} from '../Context'
+import useHover from '../hooks/useHover'
 
 function Image({className, img}) {
-    const [hovered, setHovered] = useState(false)
     const {toggleFavorite, addImageToCart, removeImageFromCart, cartItems} = useContext(Context)
+    const [hovered, ref] = useHover()
 
-    const heartIcon = hovered && <i className="ri-heart-line favorite" onClick={()=> toggleFavorite(img.id)}></i>
-    const heartIconFilled = <i className="ri-heart-fill favorite" onClick={()=> toggleFavorite(img.id)}></i>
+    function heartIcon() {
+        if (img.isFavorite) {
+            return <i className="ri-heart-fill favorite" onClick={()=> toggleFavorite(img.id)}></i>
+        } else if (hovered){
+            return <i className="ri-heart-line favorite" onClick={()=> toggleFavorite(img.id)}></i>
+        }
+    }
 
     function cartIcon() {
-        if (cartItems.some(e => e.id === img.id))  {
+        if (cartItems.some(e => e.id === img.id)) {
             return <i className="ri-shopping-cart-fill cart" onClick={()=>removeImageFromCart(img)}></i>
         }
-        else if (hovered){
+        else if (hovered) {
             return <i className="ri-add-circle-line cart" onClick={()=>addImageToCart(img)}></i>
         } 
     }
 
     return (
         <div className={`${className}  image-container`}
-             onMouseEnter={() => setHovered(true)} 
-             onMouseLeave={() => setHovered(false)}
+             ref={ref}
         >
             <img src={img.url} className="image-grid" />
-            {(img.isFavorite) ? heartIconFilled : heartIcon}
+            {heartIcon()}
             {cartIcon()}
         </div>
     )
